fd206b3fe8c6033f8384b13beb86d0f00f43b311
Cedric Champeau <cedric.champeau@gmail.com>
2022-01-20 08:35:42 +0100

    Upgrade to Micronaut 3.3.0-M1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,12 +27,6 @@ val micronautVersion = providers.gradleProperty("micronautVersion")
 
 dependencyResolutionManagement {
     repositories {
-        maven {
-            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            content {
-                includeGroup("io.micronaut")
-            }
-        }
         mavenCentral()
     }
 
