7d2188fccbec8322252815d4b92385e2c79f1989
Cedric Champeau <cedric.champeau@gmail.com>
2022-04-27 10:41:17 +0200

    Upgrade to build plugins 5.3.3.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
 }
 
 plugins {
-    id("io.micronaut.build.shared.settings") version "5.3.2"
+    id("io.micronaut.build.shared.settings") version "5.3.3"
 }
 
 enableFeaturePreview("VERSION_CATALOGS")
