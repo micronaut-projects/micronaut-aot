0edb8a0633451a7895b8c2dacc860e52c9a6f146
Dean Wette <wetted@unityfoundation.io>
2023-07-13 12:22:27 -0500

    Remove baseline version for binary compatibility check

--- aot-bom/build.gradle.kts
+++ aot-bom/build.gradle.kts
@@ -1,11 +1,3 @@
 plugins {
     id("io.micronaut.build.internal.bom")
 }
-
-micronautBuild {
-    binaryCompatibility {
-        // TODO required for now. Remove after Micronaut 4 release
-        baselineVersion.set("2.0.0-M4")
-        enabled.set(true)
-    }
-}
