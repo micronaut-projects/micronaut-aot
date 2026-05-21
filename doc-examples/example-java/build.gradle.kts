6f904795c659d9016c3386ff3a10f2890c174adb
AI on behalf of Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2026-05-21 09:32:38 +0000

    Add tests for guide doc examples

--- doc-examples/example-java/build.gradle.kts
+++ doc-examples/example-java/build.gradle.kts
@@ -10,4 +10,12 @@ dependencies {
     implementation(platform(mn.micronaut.core.bom))
     implementation(mn.micronaut.context)
     implementation(projects.micronautAotCore)
+
+    testImplementation(mnTest.junit.jupiter.api)
+    testRuntimeOnly(mnTest.junit.jupiter.engine)
+    testRuntimeOnly(mnTest.junit.platform.launcher)
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
 }
--- doc-examples/example-java/src/test/java/example/configurer/ApplicationTest.java
+++ doc-examples/example-java/src/test/java/example/configurer/ApplicationTest.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2017-2021 original authors
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -13,11 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-plugins {
-    id("io.micronaut.build.internal.parent")
-    id("io.micronaut.build.internal.dependency-updates")
-}
+package example.configurer;
 
-repositories {
-    mavenCentral()
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest {
+    @Test
+    void applicationStartsWithContextConfigurer() {
+        Application.main();
+    }
 }
